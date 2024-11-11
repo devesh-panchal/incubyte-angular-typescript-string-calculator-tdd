277871276ce410311eace9c68acd27855dd90aa7
Devesh <devucp021@gmail.com>
2024-11-11 16:48:30 +0530

    added text area input & showed resut & error to user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,41 @@ import { StringCalculatorService } from './service/string-calculator.service';
 })
 export class AppComponent {
 
+  stringCalCulator!: string;
+
   title = 'incubyte angular typescript string calculator tdd';
+  answer: number | undefined;
+  showError: any;
+  errorMessage: string | undefined;
 
   constructor(public stringCalculator: StringCalculatorService) {
 
+    this.title=this.title.split(" ").map(word=>word.split("").reverse().join("")).join(" ")
+
+
+
+  }
+
+  calculate(){
+
+    this.answer=undefined
+    this.showError=false
+
+
+    console.log(this.stringCalCulator);
+
+
+    try {
+      this.answer = this.stringCalculator.add(this.stringCalCulator)
+
+    } catch (error:any) {
+      this.showError=true
+      this.errorMessage=error
+    }
+
+    console.log("answer",this.answer);
+    
+    
   }
 
 
